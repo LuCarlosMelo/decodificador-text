02635c2c984327c01911cf477e25fca922a279ce
LuCarlosMelo <Luizcapeme@gmail.com>
2022-03-29 22:06:30 -0300

    Realizado lógica para criptografar o texto e validar se a letra está em minúsculo

--- script/criptografar.js
+++ script/criptografar.js
@@ -1,2 +1,39 @@
+var botaoCriptografar = document.querySelector('.botao__criptografar');
 
+botaoCriptografar.addEventListener('click', function(){
+    var inputCripto = document.querySelector('.criptografar__entrada');
+    var conteudoCripto = inputCripto.value;
+
+    var x = renomearCripto(conteudoCripto);
+        
+    var inputDescripto = document.querySelector('.descriptografar__entrada');
+
+    var strings = conteudoCripto;
+    var i=0;
+    
+    while (i <= strings.length){
+        
+        if (strings == strings.toLowerCase()){
+            inputDescripto.value = x;
+
+            inputCripto.value = '';     
+            i++;
+            break;
+        } else{
+            alert('Por favor, digite apenas letras em minúsculo');
+            break;
+        }
+    }    
+})
+
+function renomearCripto(conteudoCripto){
+
+    var resultadoCripto = conteudoCripto.replace(/e/gi, 'enter')
+    .replace(/i/gi, 'imes')
+    .replace(/a/gi, 'ai')
+    .replace(/o/gi, 'ober')
+    .replace(/u/gi, 'ufat');
+
+    return resultadoCripto;
+}
        
